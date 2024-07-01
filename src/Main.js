ba805b20cc70bfec7ff3f6970b84b1e0d3dfea62
mantequilla45 <vinzkenz45@gmail.com>
2024-07-01 09:43:24 +0800

    Create Main.js

---
+++ src/Main.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Cure from "./Cure";
+import BookSchedule from "./BookSchedule";
+
+const Main = ()=>{
+    return (
+        <> 
+            <BrowserRouter> 
+                <Routes>
+                    <Route index element = { <App/> }/>
+                    <Route path = "/cure" element = { <Cure/> }/>
+                    <Route path = "/bookschedule" element = { <BookSchedule/> }/>
+                </Routes>
+            </BrowserRouter>
+        </>
+
+    );
+};
+
+export default Main;
