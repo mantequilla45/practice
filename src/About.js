105315dd04693394b3068dfceec80652f3cd6301
Vince <152976699+mantequilla45@users.noreply.github.com>
2024-07-06 23:40:41 +0800

    hakdog

--- src/About.js
+++ src/About.js
@@ -1,13 +1,26 @@
-import React from "react";
-import styled from "styled-components";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import styled from 'styled-components';
+import Header from './About';
+import Footer from './About';
 
-const About = ()=> {
-    return (
-        <div>
-        <img src="https://www.wallpapers13.com/wp-content/uploads/2016/04/Rooster-with-beautiful-colorful-feathers-reddish-orange-neck-blue-gray-feathers-on-breast-tailed-black-red-crested-eyes-and-beautiful-poultry-HD-resolution-840x525.jpg" alt="cock"/>
-        </div>
-    );
-};
+const Main = styled.main`
+  width: 100%;
+  max-width: 1780px;
+`;
 
-export default About;
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const App = () => (
+  <AppWrapper>
+    <Header />
+    <Main>
+      {/* Insert components here */}
+    </Main>
+    <Footer />
+  </AppWrapper>
+);
+
+export default App;
