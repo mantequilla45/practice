8c10feecbc8689a4d630321bd72d5e8eb9df3530
Vince <152976699+mantequilla45@users.noreply.github.com>
2024-07-22 00:21:45 +0800

    fixed some bugs

--- src/UserLoggedIn.js
+++ src/UserLoggedIn.js
@@ -59,7 +59,7 @@ import React, { useEffect, useState } from 'react';
 import { signOut } from 'firebase/auth';
 import { doc, getDoc } from 'firebase/firestore';
 import { auth, data } from './firebase';
-import './UserLoggedIn.css'; // Ensure this CSS file is styled appropriately
+import './UserLoggedIn.css';
 import { useNavigate } from 'react-router-dom';
 
 const UserLoggedIn = ({ user }) => {
@@ -72,17 +72,15 @@ const UserLoggedIn = ({ user }) => {
       let profilePicUrl = '';
 
       if (user.displayName) {
-        // User logged in via third party with a display name and possibly a photoURL
         const firstName = user.displayName.split(' ')[0];
         nameToDisplay = firstName;
-        profilePicUrl = user.photoURL || ''; // Use photoURL if available
+        profilePicUrl = user.photoURL || '';
       } else {
-        // User logged in with email and password
         const userDoc = await getDoc(doc(data, 'users', user.uid));
         if (userDoc.exists()) {
           const userData = userDoc.data();
           nameToDisplay = userData.username || '';
-          profilePicUrl = userData.profilePicture || ''; // Assume profilePicture field exists
+          profilePicUrl = userData.profilePicture || '';
         }
       }
 
@@ -96,33 +94,33 @@ const UserLoggedIn = ({ user }) => {
   const handleLogOut = async () => {
     try {
       await signOut(auth);
-      console.log('User logged out'); // Debugging line
+      console.log('User logged out');
     } catch (e) {
       console.error('Error logging out: ', e);
     }
   };
 
-  const placeholderImageUrl = 'https://clipground.com/images/user-icon-vector-png-6.png'; // Replace with the actual URL of your placeholder image
+  const placeholderImageUrl = 'https://clipground.com/images/user-icon-vector-png-6.png';
 
   const navigate = useNavigate();
 
   const handleUserClick = () => {
-    console.log('User clicked');
     navigate('/profile');
   }
 
   return (
     <div className="user-info">
-      <img 
-        src={profilePicture || placeholderImageUrl} 
-        alt="User Profile" 
-        className="profile-picture" 
-      />
-      <span className="user-name" style={{fontWeight: 'bold'}} onClick={handleUserClick}>{displayName || user.email}</span>
+      <div className="user-profile-container" onClick={handleUserClick}>
+        <img 
+          src={profilePicture || placeholderImageUrl} 
+          alt="User Profile" 
+          className="profile-picture" 
+        />
+        <span className="user-name">{displayName || user.email}</span>
+      </div>
       <button className="button" onClick={handleLogOut}>Log Out</button>
     </div>
   );
 };
 
 export default UserLoggedIn;
-
